02abb71626a1d7f3aabee267918ef825a4a1532c
Ondřej Zelený <zeleny.ondrej@email.cz>
2024-05-03 15:45:45 +0200

    feature: adding argparser for easier configuration and run from terminal. Updating requirements.txt

--- get_jobs.py
+++ get_jobs.py
@@ -1,20 +1,31 @@
 from cvat_sdk.api_client import Configuration, ApiClient, exceptions
+import argparse
 
-# Configuration, for testing purposes the authentication is OFF
-config = Configuration(
-    host="http://localhost:8080/",
-    # username='YOUR_USERNAME',
-    # password='YOUR_PASSWORD'
-)
-with ApiClient(config) as api_client:
 
-    try:
-        # Query list of all jobs
-        (data, response) = api_client.jobs_api.list()
-    except exceptions.ApiException as e:
-        # Catch exceptions
-        print("Exception when trying to read list of jobs: %s\n" % e)
+def main(configuration):
+    # Configuration, for testing purposes the authentication is OFF
+    config = Configuration(
+        host=configuration['host'],
+        # username='YOUR_USERNAME',
+        # password='YOUR_PASSWORD'
+    )
+    with ApiClient(config) as api_client:
 
-    jobs = data['results']
+        try:
+            # Query list of all jobs
+            (data, response) = api_client.jobs_api.list()
+        except exceptions.ApiException as e:
+            # Catch exceptions
+            print("Exception when trying to read list of jobs: %s\n" % e)
 
-print(f"Total of {len(jobs)} jobs..")
+        jobs = data['results']
+
+    print(f"Total of {len(jobs)} jobs..")
+
+
+if __name__ == "__main__":
+    # Parse arguments
+    parser = argparse.ArgumentParser(description="Description of your program")
+    parser.add_argument('--host', type=str, default='http://localhost:8080/', help='Address of the REST API')
+    arguments = vars(parser.parse_args())
+    main(arguments)
